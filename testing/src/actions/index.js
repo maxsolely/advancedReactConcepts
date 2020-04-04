317dcaa4a5feef92f338e3b47075645a0b8875e5
Max Solomonyuk <maxsolely@gmail.com>
2020-04-04 16:04:41 +0700

    Added very simple Auth and created a HOC component to reRoute to home page if now authenticated

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types'
 
 export const saveComment = comment => {
   return {
@@ -15,4 +15,8 @@ export const fetchComments = () => {
     type: FETCH_COMMENTS,
     payload: response
   }
+}
+
+export const changeAuth = (isLoggedIn) => {
+  return { type: CHANGE_AUTH, payload: isLoggedIn }
 }
